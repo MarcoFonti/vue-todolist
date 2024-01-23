3e79e34260704596d57c3af797b4dc7aac4a8182
MarcoFonti <theemporiumg@gmail.com>
2024-01-23 19:36:09 +0100

    add MILESTONE 3

--- js/script.js
+++ js/script.js
@@ -12,26 +12,49 @@ const app = createApp ({
         return {
             // DATE ARRAY OGGETTI
             date: arrayExpense,
+            // COLLEGAMENTO CON INPUT 
+            newProduct: '',
         }
     },
 
     // METODO FUNZIONI PER ELIMINARE
     methods: {
+
+        // FUNZIONE CON PARAMETRO ID CLICCATA
         productFound(id) {
+            // METODO FILTER TOGLIERE ELEMENTI 
             const newDate = this.date.filter((element) => {
+                // VERIFICO ID CLICCATO CON ID DELL'ELEMENTO
                 if(id !== element.id){
+                    // SE NON SONO UGUALI
                     return true;
                 } else {
+                    // SE SONO UGUALI
                     return false;
                 }
             })
 
+            // RIASSEGNO DATE CON NEWDATE
             this.date = newDate;
-        }
+        },
+
+        // FUNZIONE PER AGGIUNGERE ELEMENTO
+        addProduct() {
+            // CREO NUOVO OGETTO
+            const addText = {
+                id: new Date().toISOString(),
+                done: false,
+                text: this.newProduct,
+            }
+
+            // LO METTO DENTRO A DATE
+            this.date.push(addText);
+
+            // OGNI VOLTA SVOTO INPUT
+            this.newProduct = '';
+        },
     },
-
-
-
+    
 });
 
 // COLLEGO ID 
